51d7fb351806ddc3e5dc3f7ad03106ee919f579d
Indunil1992 <indunil@adroitlogic.com>
2019-07-09 16:09:43 +0530

    9

--- test/lambda.js
+++ test/lambda.js
@@ -7,23 +7,23 @@ exports.handler = function (event, context, callback) {
     sqs.receiveAndDeleteMessages({
         QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/check.fifo`,
         AttributeNames: ['All'],
-        MaxNumberOfMessages: '10',
-        VisibilityTimeout: '50',
+        MaxNumberOfMessages: '5',
+        VisibilityTimeout: '30',
         WaitTimeSeconds: '10',
-        MessageAttributeNames: ['a', 'b']
+        MessageAttributeNames: ['']
     }, function (receivedMessages) {
         console.log("receivedMessages");
-                 console.log( receivedMessages );
+        console.log(receivedMessages);
         // implement received message filtering logic here and return filtered set of messages which 
         // are allowed to delete in the next step
         return receivedMessages;
     }, function (deleteSuccessData) {
         console.log("deleteSuccessData");
-                 console.log( deleteSuccessData );
+        console.log(deleteSuccessData);
         // implement delete success state here
     }, function (error) {
         console.log("not Success");
-                 console.log( error );
+        console.log(error);
         // implement error handling logic here
     });
 
