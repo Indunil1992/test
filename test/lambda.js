1527d48ef2da5812153682510b444811b264a79b
Indunil1992 <indunil@adroitlogic.com>
2019-07-09 18:15:33 +0530

    mk

--- test/lambda.js
+++ test/lambda.js
@@ -2,37 +2,22 @@ let AWS = require('aws-sdk');
 let SL_AWS = require('slappforge-sdk-aws');
 const sqs = new SL_AWS.SQS(AWS);
 
-
 exports.handler = function (event, context, callback) {
+    sqs.sendMessage({
+        MessageBody: 'm',
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/check`,
+        DelaySeconds: '0',
+        MessageAttributes: {}
+    }, function (data) {
+         console.log("Success");
+                 console.log( data );
+        // your logic (logging etc) to handle successful message delivery, should be here
+    }, function (error) {
+         console.log("not Success");
+                 console.log( error );
+        // your logic (logging etc) to handle failures, should be here
+    });
 
-    sqs.receiveMessage({
-        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/sample1`,
-        AttributeNames: ['All'],
-        MaxNumberOfMessages: '10',
-        VisibilityTimeout: '50',
-        WaitTimeSeconds: '10',
-        MessageAttributeNames: ['']
-    }).promise()
-        .then(receivedMsgData => {
-            if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
-                let receivedMessages = receivedMsgData.Messages;
-                receivedMessages.forEach(message => {
-                    console.log("Success message");
-                    console.log(message);
-                    // your logic to access each message through out the loop. Each message is available under variable message 
-                    // within this block
-                });
-            } else {
-                console.log("Success");
-                //console.log( data );                
-                // No messages to process
-            }
-        })
-        .catch(err => {
-            console.log("notSuccess");
-            console.log(data);
-            // error handling goes here
-        });
 
 
     callback(null, { "message": "Successfully executed ex put" });
