9ab1e4ef89b606d5352ec5a7bffca979db08bacb
Indunil1992 <indunil@adroitlogic.com>
2019-07-09 16:46:03 +0530

    0

--- test/lambda.js
+++ test/lambda.js
@@ -2,31 +2,24 @@ let AWS = require('aws-sdk');
 let SL_AWS = require('slappforge-sdk-aws');
 const sqs = new SL_AWS.SQS(AWS);
 
+
 exports.handler = function (event, context, callback) {
 
-    sqs.receiveAndDeleteMessages({
-        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/check.fifo`,
-        AttributeNames: ['All'],
-        MaxNumberOfMessages: '100',
-        VisibilityTimeout: '50',
-        WaitTimeSeconds: '10',
-        MessageAttributeNames: ['a', 's', 'q']
-    }, function (receivedMessages) {
-        console.log("receivedMessages");
-        console.log(receivedMessages);
-        // implement received message filtering logic here and return filtered set of messages which 
-        // are allowed to delete in the next step
-        return receivedMessages;
-    }, function (deleteSuccessData) {
-        console.log("deleteSuccessData");
-        console.log(deleteSuccessData);
-        // implement delete success state here
+    sqs.sendMessage({
+        MessageBody: 'do',
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/test`,
+        DelaySeconds: '0',
+        MessageAttributes: {}
+    }, function (data) {
+        console.log("Success");
+                 console.log( data );
+        // your logic (logging etc) to handle successful message delivery, should be here
     }, function (error) {
         console.log("not Success");
-        console.log(error);
-        // implement error handling logic here
+                 console.log( error );
+        // your logic (logging etc) to handle failures, should be here
     });
 
 
-    callback(null, { "message": "Successfully executed - FIFO nw" });
+    callback(null, { "message": "Successfully executed - put" });
 }
